7aca61f6101d755954e1a3087bd7fbc483df7bb0
Solomon Fenton <sfenton@protoinnovations.com>
2023-12-04 10:35:08 -0500

    added collision checking to planner script

--- planner_node/planner.cpp
+++ planner_node/planner.cpp
@@ -3,6 +3,15 @@
  * planner.c
  *
  *=================================================================*/
+
+
+// MOVE IT IMPORTS
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_state/robot_state.h>
+//
+
 #include <math.h>
 #include <random>
 #include <vector>
@@ -70,58 +79,7 @@ using std::endl;
 
 using namespace std;
 
-//*******************************************************************************************************************//
-//                                                                                                                   //
-//                                                GIVEN FUNCTIONS                                                    //
-//                                                                                                                   //
-//*******************************************************************************************************************//
-
-/// @brief 
-/// @param filepath 
-/// @return map, x_size, y_size
-tuple<double*, int, int> loadMap(string filepath) {
-	std::FILE *f = fopen(filepath.c_str(), "r");
-	if (f) {
-	}
-	else {
-		printf("Opening file failed! \n");
-		throw runtime_error("Opening map file failed!");
-	}
-	int height, width;
-	if (fscanf(f, "height %d\nwidth %d\n", &height, &width) != 2) {
-		throw runtime_error("Invalid loadMap parsing map metadata");
-	}
-	
-	////// Go through file and add to m_occupancy
-	double* map = new double[height*width];
-
-	double cx, cy, cz;
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
-			char c;
-			do {
-				if (fscanf(f, "%c", &c) != 1) {
-					throw runtime_error("Invalid parsing individual map data");
-				}
-			} while (isspace(c));
-			if (!(c == '0')) { 
-				map[y+x*width] = 1; // Note transposed from visual
-			} else {
-				map[y+x*width] = 0;
-			}
-		}
-	}
-	fclose(f);
-	return make_tuple(map, width, height);
-}
-
-// Splits string based on deliminator
-vector<string> split(const string& str, const string& delim) {   
-		// https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c/64886763#64886763
-		const std::regex ws_re(delim);
-		return { std::sregex_token_iterator(str.begin(), str.end(), ws_re, -1), std::sregex_token_iterator() };
-}
-
+// Helper functions - remnants of HW
 
 double* doubleArrayFromString(string str) {
 	vector<string> vals = split(str, ",");
@@ -142,157 +100,48 @@ bool equalDoubleArrays(double* v1, double *v2, int size) {
     return true;
 }
 
-typedef struct {
-	int X1, Y1;
-	int X2, Y2;
-	int Increment;
-	int UsingYIndex;
-	int DeltaX, DeltaY;
-	int DTerm;
-	int IncrE, IncrNE;
-	int XIndex, YIndex;
-	int Flipped;
-} bresenham_param_t;
+//*******************************************************************************************************************//
+//                                                                                                                   //
+//                                              COLLISION CHECKING                                                   //
+//                                                                                                                   //
+//*******************************************************************************************************************//
 
 
-void ContXY2Cell(double x, double y, short unsigned int* pX, short unsigned int *pY, int x_size, int y_size) {
-	double cellsize = 1.0;
-	//take the nearest cell
-	*pX = (int)(x/(double)(cellsize));
-	if( x < 0) *pX = 0;
-	if( *pX >= x_size) *pX = x_size-1;
-
-	*pY = (int)(y/(double)(cellsize));
-	if( y < 0) *pY = 0;
-	if( *pY >= y_size) *pY = y_size-1;
-}
-
-
-void get_bresenham_parameters(int p1x, int p1y, int p2x, int p2y, bresenham_param_t *params) {
-	params->UsingYIndex = 0;
-
-	if (fabs((double)(p2y-p1y)/(double)(p2x-p1x)) > 1)
-		(params->UsingYIndex)++;
-
-	if (params->UsingYIndex)
-		{
-			params->Y1=p1x;
-			params->X1=p1y;
-			params->Y2=p2x;
-			params->X2=p2y;
-		}
-	else
-		{
-			params->X1=p1x;
-			params->Y1=p1y;
-			params->X2=p2x;
-			params->Y2=p2y;
-		}
-
-	 if ((p2x - p1x) * (p2y - p1y) < 0)
-		{
-			params->Flipped = 1;
-			params->Y1 = -params->Y1;
-			params->Y2 = -params->Y2;
-		}
-	else
-		params->Flipped = 0;
-
-	if (params->X2 > params->X1)
-		params->Increment = 1;
-	else
-		params->Increment = -1;
-
-	params->DeltaX=params->X2-params->X1;
-	params->DeltaY=params->Y2-params->Y1;
-
-	params->IncrE=2*params->DeltaY*params->Increment;
-	params->IncrNE=2*(params->DeltaY-params->DeltaX)*params->Increment;
-	params->DTerm=(2*params->DeltaY-params->DeltaX)*params->Increment;
-
-	params->XIndex = params->X1;
-	params->YIndex = params->Y1;
-}
-
-void get_current_point(bresenham_param_t *params, int *x, int *y) {
-	if (params->UsingYIndex) {
-        *y = params->XIndex;
-        *x = params->YIndex;
-        if (params->Flipped)
-            *x = -*x;
+int IsValidLineSegment(double* start, double* end) {
+    // TODO: tune based on speed/accuracy constraints
+    int divisions = 10;
+    double* increment = (start - end)/10;
+    for (int i = 0; i < divisions; ++i) {
+        double* intermediate = start + i*increment;
+        if (!IsValidArmConfiguration(intermediate)) {
+            return 0;
+        }
     }
-	else {
-        *x = params->XIndex;
-        *y = params->YIndex;
-        if (params->Flipped)
-            *y = -*y;
-    }
+    return 1;
 }
 
-int get_next_point(bresenham_param_t *params) {
-	if (params->XIndex == params->X2) {
-        return 0;
-    }
-	params->XIndex += params->Increment;
-	if (params->DTerm < 0 || (params->Increment < 0 && params->DTerm <= 0))
-		params->DTerm += params->IncrE;
-	else {
-        params->DTerm += params->IncrNE;
-        params->YIndex += params->Increment;
-	}
-	return 1;
-}
+int IsValidArmConfiguration(double* state) {
+    // TODO: move robot_model_loader and planning_scene to global scope, do not neet to instantiate every time
 
+     // Load the robot model
+    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+    robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+    planning_scene::PlanningScene planning_scene(kinematic_model);
 
+    // Create a RobotState object for collision checking
+    robot_state::RobotState current_state(kinematic_model);
 
-int IsValidLineSegment(double x0, double y0, double x1, double y1, double*	map,
-			 int x_size, int y_size) {
-	bresenham_param_t params;
-	int nX, nY; 
-	short unsigned int nX0, nY0, nX1, nY1;
+    std::vector<double> joint_values = {state[0], state[1], state[2], state[3], state[4], state[5]}; // Replace with your joint values
+    current_state.setJointGroupPositions("xarm6", joint_values); // "arm" is the planning group name for your xArm6
 
-	//printf("checking link <%f %f> to <%f %f>\n", x0,y0,x1,y1);
-		
-	//make sure the line segment is inside the environment
-	if(x0 < 0 || x0 >= x_size ||
-		x1 < 0 || x1 >= x_size ||
-		y0 < 0 || y0 >= y_size ||
-		y1 < 0 || y1 >= y_size)
-		return 0;
-
-	ContXY2Cell(x0, y0, &nX0, &nY0, x_size, y_size);
-	ContXY2Cell(x1, y1, &nX1, &nY1, x_size, y_size);
-
-	//printf("checking link <%d %d> to <%d %d>\n", nX0,nY0,nX1,nY1);
-
-	//iterate through the points on the segment
-	get_bresenham_parameters(nX0, nY0, nX1, nY1, &params);
-	do {
-		get_current_point(&params, &nX, &nY);
-		if(map[GETMAPINDEX(nX,nY,x_size,y_size)] == 1)
-			return 0;
-	} while (get_next_point(&params));
-
-	return 1;
-}
-
-int IsValidArmConfiguration(double* angles, int numofDOFs, double*	map,
-			 int x_size, int y_size) {
-    double x0,y0,x1,y1;
-    int i;
-		
-	 //iterate through all the links starting with the base
-	x1 = ((double)x_size)/2.0;
-	y1 = 0;
-	for(i = 0; i < numofDOFs; i++){
-		//compute the corresponding line segment
-		x0 = x1;
-		y0 = y1;
-		x1 = x0 + LINKLENGTH_CELLS*cos(2*PI-angles[i]);
-		y1 = y0 - LINKLENGTH_CELLS*sin(2*PI-angles[i]);
-
+    // Check for collisions
+    collision_detection::CollisionRequest collision_request;
+    collision_detection::CollisionResult collision_result;
+    planning_scene.checkCollision(collision_request, collision_result, current_state);
+	
+    if(!collision_result.collision){
 		//check the validity of the corresponding line segment
-		if(!IsValidLineSegment(x0,y0,x1,y1,map,x_size,y_size))
+		if(!IsValidLineSegment(double* state, double* state_prime))
 			return 0;
 	}    
 	return 1;
